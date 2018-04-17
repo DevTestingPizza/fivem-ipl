1f00a96a5b0146782acce084c41d3d60f806504e
Vespura <31419184+TomGrobbe@users.noreply.github.com>
2018-04-17 19:05:53 +0200

    Add project files.

---
+++ IPL/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IPL
+{
+    public class Class1
+    {
+    }
+}
